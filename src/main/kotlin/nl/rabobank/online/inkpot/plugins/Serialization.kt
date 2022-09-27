df00856c1f85f5b9c3775a7a04c8fe5b5a5d0e59
giovannitedde <giovanni.tedde@rabobank.nl>
2022-09-27 14:06:15 +0200

    Works and sends to teams. TODO: Schedule jobs

--- src/main/kotlin/nl/rabobank/online/inkpot/plugins/Serialization.kt
+++ src/main/kotlin/nl/rabobank/online/inkpot/plugins/Serialization.kt
@@ -1,6 +1,8 @@
 package nl.rabobank.online.inkpot.plugins
 
 import com.fasterxml.jackson.databind.SerializationFeature
+import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
 import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.serialization.jackson.*
@@ -15,21 +17,37 @@ fun Application.configureSerialization() {
             enable(SerializationFeature.INDENT_OUTPUT)
         }
     }
-
+    val webhook = "https://raboweb.webhook.office.com/webhookb2/176d40fb-a85b-4356-8e19-214b1c895cc9@6e93a626-8aca" +
+            "-4dc1-9191-ce291b4b75a1/IncomingWebhook/0906991649b24c5489163f564dc5e725/" +
+            "2da6094e-7349-475e-b1fa-6f99bb965e9e"
     routing {
         get("/json/jackson") {
-            val response: DadJoke = client.get("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit").body()
+            val response: Gioke = client.get("https://v2.jokeapi.dev/joke/Any?" +
+                    "blacklistFlags=nsfw,religious,political,racist,sexist,explicit")
+                .body()
+            val body = TeamsBody("$response by Gio")
+            client.post(webhook){
+                setBody(Gson().toJson(body))
+            }
             call.respond(response)
         }
     }
 }
 
-data class DadJoke(
-    val categories: List<String>,
-    val created_at: String,
-    val updated_at: String,
-    val icon_url: String,
-    val id: String,
-    val url: String,
-    val value: String
-)
+data class Gioke(
+    val error: Boolean,
+    val category: String,
+    val type: String,
+    val setup: String,
+    val delivery: String,
+    val flags: Object?,
+    val id: Int,
+    val safe: Boolean,
+    val lang: String
+){
+    override fun toString(): String {
+        return "$setup $delivery"
+    }
+}
+
+data class TeamsBody(@SerializedName("text") val body: String)
