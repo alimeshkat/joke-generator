8b11d5635a56b5005f60fe0fc37afda58a536dc0
Arno Bats <arno.bats@rabobank.nl>
2022-09-27 12:52:59 +0200

    added post to teams

--- src/main/kotlin/nl/rabobank/online/inkpot/plugins/Serialization.kt
+++ src/main/kotlin/nl/rabobank/online/inkpot/plugins/Serialization.kt
@@ -28,5 +28,11 @@ fun Application.configureSerialization() {
             call.respond(response)
 
         }
+
+        post("/posttest") {
+           client.post("https://raboweb.webhook.office.com/webhookb2/176d40fb-a85b-4356-8e19-214b1c895cc9@6e93a626-8aca-4dc1-9191-ce291b4b75a1/IncomingWebhook/0906991649b24c5489163f564dc5e725/2da6094e-7349-475e-b1fa-6f99bb965e9e"){
+               setBody("{\"Text\": \"Lekker brood bakken\"}")
+           }
+        }
     }
 }
