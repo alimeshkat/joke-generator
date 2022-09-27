ef9ba0569d1527e242ce6ac56b58b3d5ff99aa86
giovannitedde <giovanni.tedde@rabobank.nl>
2022-09-27 10:57:22 +0200

    to compare

--- src/main/kotlin/nl/rabobank/online/inkpot/Application.kt
+++ src/main/kotlin/nl/rabobank/online/inkpot/Application.kt
@@ -1,12 +1,6 @@
-package nl.rabobank.online.inkpot
-
-import io.ktor.server.engine.*
-import io.ktor.server.cio.*
-import nl.rabobank.online.inkpot.plugins.*
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
 
 fun main() {
-    embeddedServer(CIO, port = 8080, host = "0.0.0.0") {
-        configureSerialization()
-        configureRouting()
-    }.start(wait = true)
+    val client = HttpClient(CIO)
 }
---
+++ src/main/kotlin/nl/rabobank/online/inkpot/plugins/KClient.kt
@@ -0,0 +1,4 @@
+package nl.rabobank.online.inkpot.plugins
+
+class KClient {
+}
